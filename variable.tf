d4bc94ab9ffdbe027fdee6ee5f5946de1fc6142e
diranana <111412969+diranana@users.noreply.github.com>
2024-03-20 13:46:01 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,24 +3,9 @@ variable "aws_region" {
   default     = "ap-southeast-2"
 }
 
-variable "key_name" {
-  description = " SSH keys to connect to ec2 instance"
-  default     =  "mysdkKey"
-}
-
 variable "instance_type" {
   description = "instance type for ec2"
   default     =  "t2.micro"
-}
-
-variable "security_group" {
-  description = "Name of security group"
-  default     = "my-jenkins-security-group"
-}
-
-variable "tag_name" {
-  description = "Tag Name of for Ec2 instance"
-  default     = "my-ec2-instance"
 }
 
 variable "ami_id" {
